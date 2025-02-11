9612f448353823d1e782c2a0c511e52193a7bf0c
joo98e <jtbeok@gmail.com>
2025-02-11 18:22:40 +0900

    Refactor: change project structure

---
+++ app/features/home/pages/index.tsx
@@ -0,0 +1,15 @@
+import { log } from "console";
+import { Button } from "~/common/components/ui/button";
+
+export default function Page() {
+  function handleClickButton() {
+    log(5);
+  }
+
+  return (
+    <div className="m-[12rem] flex flex-col gap-12 justify-center items-center">
+      <h1 className="text-4xl">🚀 Go</h1>
+      <Button onClick={handleClickButton}>Clike me!</Button>
+    </div>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,5 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("/about-us", "routes/AboutUs.tsx")
+  index("features/home/pages/index.tsx"), //
 ] satisfies RouteConfig;
