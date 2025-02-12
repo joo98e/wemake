6e3fa7a68bfc24911e7601c7b7161c92a22a55ef
joo98e <jtbeok@gmail.com>
2025-02-13 02:35:01 +0900

    feat: shadcn / navigation / content 2

--- app/common/components/ui/navigation/navigation.tsx
+++ app/common/components/ui/navigation/navigation.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle
 } from "~/common/components/ui/navigation-menu";
 import { Separator } from "~/common/components/ui/separator";
+import { cn } from "~/lib/utils";
 
 const menus = [
   {
@@ -135,14 +138,59 @@ export default function Navigation() {
           <NavigationMenuList>
             {menus.map((menu) => (
               <NavigationMenuItem key={menu.name}>
-                <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  {menu.items?.map((item) => (
-                    <NavigationMenuItem key={item.name}>
-                      <Link to={item.to}>{item.name}</Link>
-                    </NavigationMenuItem>
-                  ))}
-                </NavigationMenuContent>
+                {menu.items ? (
+                  <>
+                    <Link to={menu.to}>
+                      <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                    </Link>
+                    <NavigationMenuContent>
+                      <ul
+                        className={
+                          "grid w-[600px] font-light gap-3 p-4 grid-cols-2"
+                        }
+                      >
+                        {menu.items?.map((item) => (
+                          <NavigationMenuItem
+                            key={item.name}
+                            className={cn([
+                              "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                              item.to === "/products/promote" &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                              item.to === "/jobs/submit" &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                            ])}
+                          >
+                            <NavigationMenuLink asChild>
+                              <Link
+                                className={
+                                  "p-3 space-y-1 block leading-none no-underline outline-none"
+                                }
+                                to={item.to}
+                              >
+                                <span
+                                  className={"text-sm font-medium leading-none"}
+                                >
+                                  {item.name}
+                                </span>
+                                <p
+                                  className={
+                                    "text-sm leading-snug text-muted-foreground"
+                                  }
+                                >
+                                  {item.description}
+                                </p>
+                              </Link>
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <Link className={navigationMenuTriggerStyle()} to={menu.to}>
+                    {menu.name}
+                  </Link>
+                )}
               </NavigationMenuItem>
             ))}
           </NavigationMenuList>
