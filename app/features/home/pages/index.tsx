bc8797f73af25fc559106684b1d54df39051112f
joo98e <jtbeok@gmail.com>
2025-02-13 00:24:13 +0900

    style: delete unused code

--- app/features/home/pages/index.tsx
+++ app/features/home/pages/index.tsx
@@ -1,11 +1,4 @@
-import { log } from "console";
-import { Button } from "~/common/components/ui/button";
-
 export default function Page() {
-  function handleClickButton() {
-    log(5);
-  }
-
   return (
     <div className="m-[12rem] flex flex-col gap-12 justify-center items-center">
       <h1 className="text-4xl">🚀 Go</h1>
